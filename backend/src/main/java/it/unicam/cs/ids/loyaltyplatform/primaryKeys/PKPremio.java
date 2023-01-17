3b3947eb744ca588c73ca84f44d34f9f65356a1a
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-17 11:24:56 +0100

    [FIX] Solved some digit errors and the settings for DB connection.

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/primaryKeys/PKPremio.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/primaryKeys/PKPremio.java
@@ -1,6 +1,6 @@
 package it.unicam.cs.ids.loyaltyplatform.primaryKeys;
 
 public class PKPremio implements java.io.Serializable {
-    private Integer qualAzienda;
+    private Integer qualeAzienda;
     private Integer qualeProdotto;
 }
--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/primaryKeys/PKSconto.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/primaryKeys/PKSconto.java
@@ -1,6 +1,6 @@
 package it.unicam.cs.ids.loyaltyplatform.primaryKeys;
 
 public class PKSconto implements java.io.Serializable {
-    private Integer quale_azienda;
-    private Integer quale_prodotto;
+    private Integer qualeAzienda;
+    private Integer qualeProdotto;
 }
