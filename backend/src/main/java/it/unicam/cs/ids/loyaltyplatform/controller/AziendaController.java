eda8ad9b9ec7456e0c92267f7e6c7cf85cf91cea
carmcc <carmine.marchesani@studenti.unicam.it>
2023-01-18 11:45:42 +0100

    [ADD] added AziendaController.java (//da rivedere)

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/AziendaController.java
@@ -0,0 +1,45 @@
+package it.unicam.cs.ids.loyaltyplatform.controller;
+import it.unicam.cs.ids.loyaltyplatform.entity.Azienda;
+import it.unicam.cs.ids.loyaltyplatform.service.AziendaService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/azienda")
+public class AziendaController
+{
+    private final AziendaService aziendaService;
+    public AziendaController(AziendaService aziendaService) {
+        this.aziendaService = aziendaService;
+    }
+
+    @RequestMapping(value = "/getAziendaById/{id}", method = RequestMethod.GET)
+    public Azienda getAziendaById(@PathVariable Integer id) {
+        return this.aziendaService.getAziendaById(id);
+    }
+
+    @RequestMapping(value = "/getAllAziende", method = RequestMethod.GET)
+    public List<Azienda> getAllAziende() {
+        return this.aziendaService.getAllAziende();
+    }
+
+    @RequestMapping(value = "/addAzienda", method = RequestMethod.POST)
+    public Azienda addAzienda(Azienda azienda) {
+        return this.aziendaService.addAzienda(azienda);
+    }
+
+    @RequestMapping(value = "/deleteAziendaById/{id}", method = RequestMethod.DELETE)
+    public void deleteAziendaById(@PathVariable Integer id) {
+        this.aziendaService.deleteAziendaById(id);
+    }
+
+    @RequestMapping(value = "/updateAzienda", method = RequestMethod.PUT)
+    public void updateAzienda(Azienda azienda) {
+        this.aziendaService.updateAzienda(azienda);
+    }
+
+}
