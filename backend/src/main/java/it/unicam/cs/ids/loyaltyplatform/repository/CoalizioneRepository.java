3c78186f0083237aba72a0b88a5fbfab8cbed144
carmcc <carmine.marchesani@studenti.unicam.it>
2023-01-19 17:55:34 +0100

    [FIX] fixed type parameter in CoalizioneRepository.java

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/CoalizioneRepository.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/CoalizioneRepository.java
@@ -3,6 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import it.unicam.cs.ids.loyaltyplatform.entity.Coalizione;
 @Repository
-public interface CoalizioneRepository extends JpaRepository<CoalizioneRepository, Integer> {
+public interface CoalizioneRepository extends JpaRepository<Coalizione, Integer> {
 }
 
