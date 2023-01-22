ee678c5ffb0d960d74d95b5e09f4f6b187142982
carmcc <carmine.marchesani@studenti.unicam.it>
2023-01-22 10:51:51 +0100

    [UPDATE] added Repository tag

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/PremioRepository.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/PremioRepository.java
@@ -1,7 +1,9 @@
 package it.unicam.cs.ids.loyaltyplatform.repository;
 import it.unicam.cs.ids.loyaltyplatform.entity.Premio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PremioRepository extends JpaRepository<Premio, Integer> {
 }
 
