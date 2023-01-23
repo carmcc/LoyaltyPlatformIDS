0a92f3272307233a7a3241e68131158c6f607182
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-23 09:48:06 +0100

    [ADD] Added RecensioneService.java and RecensioneController.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/RecensioneController.java
@@ -0,0 +1,31 @@
+package it.unicam.cs.ids.loyaltyplatform.controller;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Recensione;
+import it.unicam.cs.ids.loyaltyplatform.service.RecensioneService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/recensione")
+public class RecensioneController {
+    final RecensioneService recensioneService;
+    public RecensioneController(RecensioneService recensioneService) {this.recensioneService = recensioneService;}
+    @GetMapping("/getRecensioniByIdConsumatore/{id}")
+    public List<Recensione> getRecensioniByIdConsumatore(@PathVariable("id") Integer id) {return this.recensioneService.getRecensioniByIdConsumatore(id);}
+    @GetMapping("/getRecensioniByIdAzienda/{id}")
+    public List<Recensione> getRecensioniByIdAzienda(@PathVariable("id") Integer id) {return this.recensioneService.getRecensioniByIdAzienda(id);}
+    @GetMapping("/getRecensioneByConsumatoreAndAzienda/{idConsumatore}&{idAzienda}")
+    public Optional<Recensione> getRecensioneByConsumatoreAndAzienda(@PathVariable("idConsumatore") Integer idConsumatore, @PathVariable("idAzienda") Integer idAzienda) {
+        return this.recensioneService.getRecensioneByConsumatoreAndAzienda(idConsumatore,idAzienda);
+    }
+    @GetMapping("/getAllRecensioni")
+    public List<Recensione> getAllRecensioni() {return this.recensioneService.getAllRecensioni();}
+    @PostMapping("/AddRecensione")
+    public Recensione AddRecensione(@RequestBody Recensione recensione) {return this.recensioneService.AddRecensione(recensione);}
+    @DeleteMapping("/deleteRecensione")
+    public void deleteRecensione(@RequestBody Recensione recensione) {this.recensioneService.deleteRecensione(recensione);}
+    @PutMapping("/updateRecensione")
+    public void updateRecensione(@RequestBody Recensione recensione) {this.recensioneService.updateRecensione(recensione);}
+}
---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/service/RecensioneService.java
@@ -0,0 +1,27 @@
+package it.unicam.cs.ids.loyaltyplatform.service;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Recensione;
+import it.unicam.cs.ids.loyaltyplatform.repository.RecensioneRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecensioneService {
+    final RecensioneRepository recensioneRepository;
+    public RecensioneService(RecensioneRepository recensioneRepository) {this.recensioneRepository = recensioneRepository;}
+    public List<Recensione> getRecensioniByIdConsumatore(Integer id) {
+        return this.recensioneRepository.findAll().stream().filter(x->x.getQualeConsumatore()==id.intValue()).toList();
+    }
+    public List<Recensione> getRecensioniByIdAzienda(Integer id) {
+        return this.recensioneRepository.findAll().stream().filter(x->x.getQualeAzienda()==id.intValue()).toList();
+    }
+    public Optional<Recensione> getRecensioneByConsumatoreAndAzienda(Integer idConsumatore, Integer idAzienda) {
+        return getRecensioniByIdConsumatore(idConsumatore).stream().filter(x->x.getQualeAzienda()==idAzienda.intValue()).findAny();
+    }
+    public List<Recensione> getAllRecensioni() {return this.recensioneRepository.findAll();}
+    public Recensione AddRecensione(Recensione recensione) {return this.recensioneRepository.save(recensione);}
+    public void deleteRecensione(Recensione recensione) {this.recensioneRepository.delete(recensione);}
+    public void updateRecensione(Recensione recensione) {this.recensioneRepository.saveAndFlush(recensione);}
+}
