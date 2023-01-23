beb4ae59e1b6c80ffcf4e2ed7f030609f34f5a5b
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-23 11:13:26 +0100

    [ADD] Added ScontoService.java and ScontoController.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ScontoController.java
@@ -0,0 +1,35 @@
+package it.unicam.cs.ids.loyaltyplatform.controller;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Sconto;
+import it.unicam.cs.ids.loyaltyplatform.service.ScontoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/sconto")
+public class ScontoController {
+    private final ScontoService scontoService;
+    public ScontoController(ScontoService scontoService) {this.scontoService = scontoService;}
+    @GetMapping("/getScontiByIdAzienda/{id}")
+    public List<Sconto> getScontiByIdAzienda(@PathVariable("id") Integer id) {
+        return this.scontoService.getScontiByIdAzienda(id);
+    }
+    @GetMapping("/getScontiByIdProdotto/{id}")
+    public List<Sconto> getScontiByIdProdotto(@PathVariable("id") Integer id) {
+        return this.scontoService.getScontiByIdProdotto(id);
+    }
+    @GetMapping("/getScontoByAziendaAndProdotto/{idAzienda}&{idProdotto}")
+    public Optional<Sconto> getScontoByAziendaAndProdotto(@PathVariable("idAzienda") Integer idAzienda,@PathVariable("idProdotto") Integer idProdotto) {
+        return this.scontoService.getScontoByAziendaAndProdotto(idAzienda, idProdotto);
+    }
+    @GetMapping("/getAllSconti")
+    public List<Sconto> getAllSconti() {return this.scontoService.getAllSconti();}
+    @PostMapping("/AddSconto")
+    public Sconto AddSconto(@RequestBody Sconto sconto) {return this.scontoService.AddSconto(sconto);}
+    @DeleteMapping("/deleteSconto")
+    public void deleteSconto(@RequestBody Sconto sconto) {this.scontoService.deleteSconto(sconto);}
+    @PutMapping("/updateSconto")
+    public void updateSconto(@RequestBody Sconto sconto) {this.scontoService.updateSconto(sconto);}
+}
---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ScontoService.java
@@ -0,0 +1,29 @@
+package it.unicam.cs.ids.loyaltyplatform.service;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Sconto;
+import it.unicam.cs.ids.loyaltyplatform.repository.ScontoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScontoService {
+    private final ScontoRepository scontoRepository;
+    public ScontoService(ScontoRepository scontoRepository) {this.scontoRepository = scontoRepository;}
+    public List<Sconto> getScontiByIdAzienda(Integer id) {
+        return this.scontoRepository.findAll().stream().filter(x->x.getQualeAzienda()==id.intValue()).toList();
+    }
+    public List<Sconto> getScontiByIdProdotto(Integer id) {
+        return this.scontoRepository.findAll().stream().filter(x->x.getQualeProdotto()==id.intValue()).toList();
+    }
+    public Optional<Sconto> getScontoByAziendaAndProdotto(Integer idAzienda, Integer idProdotto) {
+        return this.scontoRepository.findAll().stream()
+                .filter(x-> x.getQualeAzienda()==idAzienda.intValue() && x.getQualeProdotto()==idProdotto.intValue())
+                .findAny();
+    }
+    public List<Sconto> getAllSconti() {return this.scontoRepository.findAll();}
+    public Sconto AddSconto(Sconto sconto) {return this.scontoRepository.save(sconto);}
+    public void deleteSconto(Sconto sconto) {this.scontoRepository.delete(sconto);}
+    public void updateSconto(Sconto sconto) {this.scontoRepository.saveAndFlush(sconto);}
+}
