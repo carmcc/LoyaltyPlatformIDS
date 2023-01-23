0f22e95a681a0bf93ea70ddb34f5cad705018b2a
carmcc <carmine.marchesani@studenti.unicam.it>
2023-01-23 21:39:49 +0100

    [ADD] added ProdottoService.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ProdottoService.java
@@ -0,0 +1,44 @@
+package it.unicam.cs.ids.loyaltyplatform.service;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Prodotto;
+import it.unicam.cs.ids.loyaltyplatform.repository.ProdottoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdottoService
+{
+    private final ProdottoRepository prodottoRepository;
+
+    public ProdottoService(ProdottoRepository prodottoRepository)
+    {
+        this.prodottoRepository = prodottoRepository;
+    }
+
+    public Prodotto getProdottoById(Integer id)
+    {
+        return this.prodottoRepository.findById(id).orElse(null);
+    }
+
+    public List<Prodotto> getAllProdotti()
+    {
+        return this.prodottoRepository.findAll();
+    }
+
+    public Prodotto addProdotto(Prodotto prodotto)
+    {
+        return this.prodottoRepository.save(prodotto);
+    }
+
+    public void deleteProdottoById(Integer id)
+    {
+        this.prodottoRepository.deleteById(id);
+    }
+
+    public void updateProdotto(Prodotto prodotto)
+    {
+        this.prodottoRepository.saveAndFlush(prodotto);
+    }
+
+}
