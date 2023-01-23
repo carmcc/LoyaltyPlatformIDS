1f5c4e78deac0fe66180441c2e029573ecf012b2
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-01-23 15:41:00 +0100

    [ADD] add ConsumatoreController.java and ConsumatoreService.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ConsumatoreController.java
@@ -0,0 +1,44 @@
+package it.unicam.cs.ids.loyaltyplatform.controller;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Consumatore;
+import it.unicam.cs.ids.loyaltyplatform.service.ConsumatoreService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consumatore")
+public class ConsumatoreController {
+    private final ConsumatoreService consumatoreService;
+    public ConsumatoreController(ConsumatoreService consumatoreService) {
+        this.consumatoreService = consumatoreService;
+    }
+
+    @GetMapping(value = "/getConsumatoreById/{id}")
+    public Consumatore getConsumatoreById(@PathVariable("id") Integer id) {
+        return this.consumatoreService.getConsumatoreById(id);
+    }
+
+    @GetMapping(value = "/getAllConsumatori")
+    public List<Consumatore> getAllConsumatori() {
+        return this.consumatoreService.getAllConsumatori();
+    }
+
+    @RequestMapping(value = "/addConsumatore", method = RequestMethod.POST)
+    public Consumatore addConsumatore(@RequestBody Consumatore consumatore) {
+        return this.consumatoreService.addConsumatore(consumatore);
+    }
+
+    @DeleteMapping(value = "/deleteConsumatoreById/{id}")
+    public void deleteConsumatoreById(@PathVariable("id") Integer id) {
+        this.consumatoreService.deleteConsumatoreById(id);
+    }
+
+//TODO da rivedere
+
+//    @PutMapping(value = "/updateConsumatore")
+//    public void updateConsumatore(@RequestBody Consumatore consumatore) {
+//        this.consumatoreService.updateConsumatore(consumatore);
+//    }
+
+}
