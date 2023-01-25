1de12a83f9688734c2d55a3624228a738ca9f028
carmcc <carmine.marchesani@studenti.unicam.it>
2023-01-25 15:49:09 +0100

    [ADD] added PermessoController.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/PermessoController.java
@@ -0,0 +1,38 @@
+package it.unicam.cs.ids.loyaltyplatform.controller;
+
+import it.unicam.cs.ids.loyaltyplatform.entity.Permesso;
+import it.unicam.cs.ids.loyaltyplatform.service.PermessoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/permesso")
+public class PermessoController
+{
+    private final PermessoService permessoService;
+
+    public PermessoController(PermessoService permessoService) {
+        this.permessoService = permessoService;
+    }
+
+    @GetMapping(value = "/getPermessoById/{id}")
+    public Permesso getPermessoById(@PathVariable("id") String id) {
+        return this.permessoService.getPermessoById(id);
+    }
+
+    @GetMapping(value = "/getAllPermessi")
+    public List<Permesso> getAllPermessi() {
+        return this.permessoService.getAllPermessi();
+    }
+
+    @PostMapping(value = "/addPermesso")
+    public Permesso addPermesso(@RequestBody Permesso permesso) {
+        return this.permessoService.addPermesso(permesso);
+    }
+
+    @DeleteMapping(value = "/deletePermessoById/{id}")
+    public void deletePermessoById(@PathVariable("id") String id) {
+        this.permessoService.deletePermessoById(id);
+    }
+}
