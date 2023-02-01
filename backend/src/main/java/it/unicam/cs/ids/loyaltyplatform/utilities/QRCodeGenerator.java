fb284c60b3125e313896d2bf1059748306a70155
carmcc <carmine.marchesani@studenti.unicam.it>
2023-02-01 17:07:05 +0100

    [ADD] added QRCodeGenerator.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/utilities/QRCodeGenerator.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.ids.loyaltyplatform.utilities;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+
+public class QRCodeGenerator
+{
+
+    public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+        QRCodeWriter toConvertImage = new QRCodeWriter();
+        BitMatrix bitMatrix = toConvertImage.encode(barcodeText, BarcodeFormat.QR_CODE, 300, 300);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+}
