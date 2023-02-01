71446445f8c758795cfbb2fc0aa92ea39da60e61
carmcc <carmine.marchesani@studenti.unicam.it>
2023-02-01 17:07:16 +0100

    [ADD] added QRCodeService.java

---
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/utilities/QRCodeService.java
@@ -0,0 +1,23 @@
+package it.unicam.cs.ids.loyaltyplatform.utilities;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class QRCodeService
+{
+    public ResponseEntity<BufferedImage> qrCodeGenerator(String qrcode) throws Exception {
+        return ResponseEntity.ok(QRCodeGenerator.generateQRCodeImage(qrcode));
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
+}
