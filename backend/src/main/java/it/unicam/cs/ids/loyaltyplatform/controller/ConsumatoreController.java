8d39fc3ae8bff30db812bc91b9832f11b0c00df7
carmcc <carmine.marchesani@studenti.unicam.it>
2023-02-02 12:09:35 +0100

    [UPDATE] QR Code generation method for url mapping implemented

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ConsumatoreController.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ConsumatoreController.java
@@ -2,16 +2,20 @@ package it.unicam.cs.ids.loyaltyplatform.controller;
 
 import it.unicam.cs.ids.loyaltyplatform.entity.Consumatore;
 import it.unicam.cs.ids.loyaltyplatform.service.ConsumatoreService;
+import it.unicam.cs.ids.loyaltyplatform.service.QRCodeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 @RestController
 @RequestMapping("/consumatore")
 @AllArgsConstructor
-public class ConsumatoreController extends EntityValidator{
+public class ConsumatoreController extends EntityValidator {
     private final ConsumatoreService consumatoreService;
+    private final QRCodeService qrCodeService;
 
     @GetMapping(value = "/getConsumatoreById/{id}")
     public Consumatore getConsumatoreById(@PathVariable("id") Integer id) {
@@ -42,5 +46,10 @@ public class ConsumatoreController extends EntityValidator{
         if(getConsumatoreById(consumatore.getIdConsumatore())==null)
             throw new IllegalArgumentException("il record da aggiornare non esiste");
         this.consumatoreService.updateConsumatore(consumatore);
+    }
+    @GetMapping(value = "/qrcode/{id}")
+    public ResponseEntity<BufferedImage> getCartaFedelta(@PathVariable("id") Integer id) throws Exception {
+        Consumatore consumatore = getConsumatoreById(id);
+        return qrCodeService.qrCodeGenerator(consumatore.getIdConsumatore().toString());
     }
 }
