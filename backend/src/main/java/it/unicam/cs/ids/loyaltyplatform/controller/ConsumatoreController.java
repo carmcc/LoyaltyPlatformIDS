141f29a4892a390846e063c9cc65a745503890ef
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-02-07 11:10:22 +0100

    [UPDATE] add tag CROSSORIGIN for ConsumatoreController.java

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ConsumatoreController.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ConsumatoreController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/consumatore")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ConsumatoreController extends EntityValidator {
     private final ConsumatoreService consumatoreService;
     private final QRCodeService qrCodeService;
