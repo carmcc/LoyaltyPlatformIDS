8378d6f1bc8d0d7c6cb67bc14fd911cb26edb48e
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-02-13 10:13:10 +0100

    [UPDATE] modify db structure

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/LoyaltyPlatformApplication.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/LoyaltyPlatformApplication.java
@@ -2,8 +2,15 @@ package it.unicam.cs.ids.loyaltyplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+//@Configuration
+//@ComponentScan(basePackages = "it.unicam.cs.ids.loyaltyplatform.service")
+//@ComponentScan(basePackages = "it.unicam.cs.ids.loyaltyplatform.controller")
+//@ComponentScan(basePackages = "it.unicam.cs.ids.loyaltyplatform.repository")
+//@ComponentScan(basePackages = "it.unicam.cs.ids.loyaltyplatform.trigger")
 public class LoyaltyPlatformApplication {
 
     public static void main(String[] args) {
