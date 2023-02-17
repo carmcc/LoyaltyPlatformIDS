df1b3e6715f081b83c227fe7c414120d9adaa53f
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-02-17 15:32:40 +0100

    [UPDATE] add new query getBuonoScontoByQualeConsumatore

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/BuonoScontoRepository.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/BuonoScontoRepository.java
@@ -3,6 +3,9 @@ import it.unicam.cs.ids.loyaltyplatform.entity.BuonoSconto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BuonoScontoRepository extends JpaRepository<BuonoSconto, Integer> {
+    List<BuonoSconto> getBuonoScontoByQualeConsumatore(Integer idConsumatore);
 }
