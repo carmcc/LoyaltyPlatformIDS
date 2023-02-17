7563613c1246eee656a63485407ce7f5a229e91a
MrError404 <patryk.bialowas@studenti.unicam.it>
2023-02-17 15:32:06 +0100

    [UPDATE] implements private method for remove expired BuonoSconto on BuonoScontoService.java

--- backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/service/BuonoScontoService.java
+++ backend/src/main/java/it/unicam/cs/ids/loyaltyplatform/service/BuonoScontoService.java
@@ -6,7 +6,6 @@ import it.unicam.cs.ids.loyaltyplatform.entity.Coalizione;
 import it.unicam.cs.ids.loyaltyplatform.repository.BuonoScontoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -26,12 +25,14 @@ public class BuonoScontoService {
     }
     public BuonoSconto addBuonoSconto(BuonoSconto buonoSconto) {
         buonoSconto.setIdBuono(null);
+        checkBuoniSconto(buonoSconto.getQualeConsumatore());
         return this.buonoScontoRepository.save(buonoSconto);
     }
     public void deleteBuonoScontoById(Integer id) {
         this.buonoScontoRepository.deleteById(id);
     }
     public void updateBuonoSconto(BuonoSconto buonoSconto) {
+        checkBuoniSconto(buonoSconto.getQualeConsumatore());
         this.buonoScontoRepository.saveAndFlush(buonoSconto);
     }
 
@@ -44,6 +45,7 @@ public class BuonoScontoService {
      * @param spesa La spesa effettuata, da cui si ricaveranno i punti
      */
     public BuonoSconto createNewBuonoSconto(Integer idConsumatore, Azienda azienda, Float spesa) {
+        checkBuoniSconto(idConsumatore);
         Calendar dataCreazione = Calendar.getInstance(Locale.ITALY);
         Calendar dataScadenza = Calendar.getInstance(Locale.ITALY);
         dataScadenza.set(dataScadenza.get(Calendar.YEAR),dataScadenza.get(Calendar.MONTH),
@@ -58,5 +60,19 @@ public class BuonoScontoService {
         float valore = spesa/coalizione.getParametroBuoniSpesa();
 
         return new BuonoSconto(null,idConsumatore,aziendaBersaglio.getIdAzienda(),dataCreazione.getTime(),dataScadenza.getTime(),valore);
+    }
+
+    /**
+     * Questo metodo controlla se i buoni sconto di un consumatore sono scaduti e li elimina.
+     *
+     * @param idConsumatore Il consumatore di cui controllare i buoni sconto
+     */
+    private void checkBuoniSconto(Integer idConsumatore) {
+        List<BuonoSconto> listaBuoniSconto = buonoScontoRepository.getBuonoScontoByQualeConsumatore(idConsumatore);
+        for(BuonoSconto buonoSconto : listaBuoniSconto) {
+                if(buonoSconto.getDataScadenza().before(Calendar.getInstance(Locale.ITALY).getTime())) {
+                    this.deleteBuonoScontoById(buonoSconto.getIdBuono());
+            }
+        }
     }
 }
